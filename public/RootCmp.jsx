67e0ed7cee713f0a8fa3382dddf1a9453996ef1f
Shany Baruch <baruchshany@gmail.com>
2025-11-17 15:47:36 +0200

    Basic login-signup

--- public/RootCmp.jsx
+++ public/RootCmp.jsx
@@ -1,6 +1,10 @@
 const Router = ReactRouterDOM.HashRouter
 const { Route, Routes } = ReactRouterDOM
+const { useState } = React
 
+import { authService } from './services/auth.service.js'
+import { LoginSignup } from './pages/LoginSignup.jsx'
+import { UserDetails } from './pages/UserDetails.jsx'
 import { UserMsg } from './cmps/UserMsg.jsx'
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { AppFooter } from './cmps/AppFooter.jsx'
@@ -8,21 +12,28 @@ import { Home } from './pages/Home.jsx'
 import { BugIndex } from './pages/BugIndex.jsx'
 import { BugDetails } from './pages/BugDetails.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
+import { NotFound } from './cmps/NotFound.jsx'
 
 export function App() {
-    return <Router>
-        <div className="app-wrapper">
-            <UserMsg />
-            <AppHeader />
-            <main className="container">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/bug" element={<BugIndex />} />
-                    <Route path="/bug/:bugId" element={<BugDetails />} />
-                    <Route path="/about" element={<AboutUs />} />
-                </Routes>
-            </main>
-            <AppFooter />
-        </div>
-    </Router>
+    const [loggedinUser, setLoggedinUser] = useState(authService.getLoggedinUser())
+    return (
+        <Router>
+            <div className="app-wrapper">
+                <UserMsg />
+                <AppHeader loggedinUser={loggedinUser} setLoggedinUser={setLoggedinUser} />
+                <main className="container">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/bug" element={<BugIndex />} />
+                        <Route path="/bug/:bugId" element={<BugDetails />} />
+                        <Route path="/about" element={<AboutUs />} />
+                        <Route path="/auth" element={<LoginSignup setLoggedinUser={setLoggedinUser} />} />
+                        <Route path="/user/:userId" element={<UserDetails />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </main>
+                <AppFooter />
+            </div>
+        </Router>
+    )
 }
--- public/cmps/AppHeader.jsx
+++ public/cmps/AppHeader.jsx
@@ -1,12 +1,42 @@
-const { NavLink } = ReactRouterDOM
+import { authService } from './services/auth.service.js'
 
-export function AppHeader() {
-    return <header className="app-header main-content single-row">
-        <h1>Miss Bug</h1>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/bug">Bugs</NavLink>
-            <NavLink to="/about">About</NavLink>
-        </nav>
-    </header>
+const { Link, NavLink, useNavigate } = ReactRouterDOM
+
+export function AppHeader({ loggedinUser, setLoggedinUser }) {
+
+    const navigate = useNavigate()
+
+    function onLogout() {
+        authService.logout()
+            .then(() => {
+                setLoggedinUser(null)
+            })
+            .catch(err => {
+                console.log(err)
+                showErrorMsg(`Couldn't logout`)
+            })
+    }
+
+    function onBack() {
+        navigate(-1)
+    }
+
+    return (
+        <header className="app-header main-content single-row">
+            <h1>Miss Bug</h1>
+            <nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/bug">Bugs</NavLink>
+                <NavLink to="/about">About</NavLink>
+                {
+                    !loggedinUser
+                        ? <NavLink to="/auth" >Login</NavLink>
+                        : <div className="user">
+                            <Link to={`/user/${loggedinUser._id}`}>{loggedinUser.fullname}</Link>
+                            <button onClick={onLogout}>logout</button>
+                        </div>
+                }
+            </nav>
+        </header>
+    )
 }
